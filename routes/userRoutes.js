13ef85a45acff23a5446e647d795a4b60b1b6ca6
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-08-20 18:12:59 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { register, login, usersController, deleteUsersController, forgotPassword } from "../controller/users-controllers.js";
+import { register, login, usersController, deleteUsersController, forgotPassword, resetPassword, myProfile, logOut, googleLogout } from "../controller/users-controllers.js";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 import passport from 'passport';
 const router = express.Router()
@@ -16,32 +16,28 @@ router.post("/login", login)
 router.get(
     "/google",
     passport.authenticate("google", {
-        scope: ["profile","email",'openid',
-         'https://www.googleapis.com/auth/user.addresses.read',
-          'https://www.googleapis.com/auth/user.phonenumbers.read']
+        scope: ["profile","email",
+        //  'https://www.googleapis.com/auth/user.addresses.read',
+        //   'https://www.googleapis.com/auth/user.phonenumbers.read'
+        ]
     }))
 
-router.get("/google/callback", passport.authenticate('google', { failureRedirect: '/login' }),
-(req, res) => {
-  const { _id, email, name, number, address } = req.user;
-
-  // Generate JWT token
-  const token = jwt.sign({ _id, email, name, number, address }, process.env.JWT_SECRET, { expiresIn: "365d", });
-
-  // Save JWT token in local storage
-  res.cookie("userID", token ,{ httpOnly: true});
-//   console.log(token)
-  res.redirect('http://localhost:3000');
-}
+router.get("/google/callback", passport.authenticate('google',
+ {  successRedirect: "http://localhost:3000",}),
 );
 
 // --------------------------------------------------------- google authentication ends here
    
     router.post("/register", register)
     router.post("/forgotPassword", forgotPassword)
-router.get("/getUser", usersController)
+    router.put("/resetPassword", resetPassword)
+router.get("/getUser",requireSignIn,isAdmin, usersController)
 
-router.delete("/delete-users", deleteUsersController)
+router.get("/logout",requireSignIn,logOut)
+router.get("/googleLogout",requireSignIn,googleLogout)
+router.get("/myProfile",requireSignIn,myProfile)
+
+router.delete("/delete-users",requireSignIn, deleteUsersController)
 
 // protected routes
 router.get("/user-auth", requireSignIn, (req, res) => {
@@ -49,9 +45,7 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 })
 
 // admin routes
-router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
-    res.status(200).send({ ok: true })
-})
+
 
 
 
