f4a973afe7520fbbe7af6862b54dfedca22679d9
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-08-20 18:11:49 +0530

    Update Foods.js

--- routes/Foods.js
+++ routes/Foods.js
@@ -1,22 +1,22 @@
-import  express  from "express";
+import express from "express";
 import { requireSignIn, isAdmin, } from "../middleware/authMiddleware.js";
 
-import { FoodController, deleteFoodController, foodGetController,  updateFoodController } from "../controller/Foods-controller.js";
+import { FoodController, deleteFoodController, foodGetController, updateFoodController } from "../controller/Foods-controller.js";
 
 
 
-const router =  express.Router()
-
-  
-  
-  
-           router.post("/create-foods", requireSignIn, isAdmin, FoodController )     //add admin here later
-            router.delete("/delete-foods/:id",requireSignIn, isAdmin, deleteFoodController )   //add admin here later
+const router = express.Router()
 
 
-              router.put("/update-foods/:id", updateFoodController )   //add admin here later
 
-              router.get("/foods", foodGetController )
+
+router.post("/create-foods", requireSignIn, isAdmin, FoodController)     //add admin here later
+router.delete("/delete-foods/:id", requireSignIn, isAdmin, deleteFoodController)   //add admin here later
+
+
+router.put("/update-foods/:id", requireSignIn, isAdmin, updateFoodController)   //add admin here later
+
+router.get("/foods", foodGetController)
 
 
 export default router
